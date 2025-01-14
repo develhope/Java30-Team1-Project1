d06a85327b494a229f4c30e82a6636462347eb82
merry <rozzimario18@gmail.com>
2025-01-14 16:52:52 +0100

    new branch for feature Antipasti.java

--- src/Antipasti.java
+++ src/Antipasti.java
@@ -1,14 +1,14 @@
 public class Antipasti {
     private String nome;
-    private Integer prezzo;
+    private Double prezzo;
 
     //TODO creare il costruttore, fare getter e setter ed un metodo per stampare le informazioni (nome e prezzo)
-    public Antipasti(String nome, Integer prezzo){
+    public Antipasti(String nome, Double prezzo){
         this.nome = nome;
         this.prezzo = prezzo;
     }
 
     public void stampaInfo(){
-
+        System.out.println("Piatto:"+nome+ " / "+ "Prezzo:"+prezzo+"€");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String [] args){
         //TODO creare gli oggetti del menu. per ogni oggetto stampare l'informazione
+        Antipasti crostini = new Antipasti("Crostini con pâté di fegato", 1.80);
+        Antipasti involtini = new Antipasti("Involtini di bresaola con rucola e scaglie di parmigiano", 2.10);
+        Antipasti fonduta = new Antipasti("Sformato di carne con fonduta", 3.00);
+        Antipasti salumi = new Antipasti("Salumi misti con formaggi stagionati", 2.50);
+        Antipasti carpaccio = new Antipasti("Carpaccio di manzo con rucola e limone", 5.30);
 
+        crostini.stampaInfo();
+        involtini.stampaInfo();
+        fonduta.stampaInfo();
+        salumi.stampaInfo();
+        carpaccio.stampaInfo();
     }
 }
